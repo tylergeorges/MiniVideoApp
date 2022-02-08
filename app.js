edf5fc907a06f7b273159442cf5c4c0930aecbeb
tylergeorges <tmg320v@gmail.com>
2022-02-08 09:58:43 -0600

    build2

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ class App extends React.Component {
         apiKey: 'apikey=423771dd',
         query: '&t=', //* t = title
         movieTitle: '',
-        searchURL: ''
+        searchURL: '',
+        movie: {}
     }
 
     handleChange = (e) =>{
@@ -28,8 +29,11 @@ class App extends React.Component {
         }, () => {
             fetch(this.state.searchURL)//* after its going to fetch the search URL from above
                 .then(response => response.json())//* takes URL and puts it in json format
-                .then(movies => console.log(movies))//* shows the movie searched in the console
-            .catch(error => console.error(error))
+                .then(movie => {
+                    this.setState({movie: movie})
+                    console.log(this.state.movie)//* set movie in the state
+                })
+            .catch(error => console.error(error)) //* handle all/any errors
         })
     }
 
@@ -42,14 +46,34 @@ class App extends React.Component {
                 <label htmlFor="movieTitle">Movie Title</label>
                 
                 {/* //* sets movie title value to what was typed and executes handle change function */}
-                <input type="text" id="movieTitle" value={this.state.movieTitle} onChange={this.handleChange}/> 
+                <input 
+                type="text" 
+                id="movieTitle" 
+                value={this.state.movieTitle} 
+                onChange={this.handleChange}/> 
 
                 <input type="submit"/>
             </form>
 
+            {/* //* setting prop for movie function which will be movie */}
+            <MovieInfo movie={this.state.movie}/>  
+
             </div>
         )
     }
+}
+
+//* when you dont need to save to state use FUNCTIONAL component
+const MovieInfo = (props) => {
+    return(
+        <div>
+        <h1>Movie Title: {props.movie.Title}</h1>
+        <h2>Year: {props.movie.Year}</h2>
+        <img src={props.movie.Poster}/>
+        <h3>Genre: {props.movie.Genre}</h3>
+        <p>{props.movie.Plot}</p>
+        </div>
+    )   
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
